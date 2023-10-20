b39035c33ec6d8f97519c7eec7c65ae7649dafc9
GoranMarjanovic90 <goranmarjanovic1990@gmail.com>
2023-10-20 22:28:16 +0200

    Server time and new threads!

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,6 +8,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.apache.commons.net.ntp.NTPUDPClient;
+import org.apache.commons.net.ntp.TimeInfo;
+
+import java.io.IOException;
+import java.net.InetAddress;
 import java.util.Date;
 import java.util.Locale;
 
@@ -28,19 +33,46 @@ public class MainActivity extends AppCompatActivity {
         updateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 Date systemDate = new Date();
                 SimpleDateFormat systemDateFormat = new SimpleDateFormat("HH:mm:ss", Locale.getDefault());
                 String formattedSystemTime = systemDateFormat.format(systemDate);
-
-
                 systemTimeTextView.setText("System Time: " + formattedSystemTime);
 
-
-                String ntpTime = "NTP Time: ";
-
-                ntpTimeTextView.setText(ntpTime);
+                // Start a background thread to fetch NTP time
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        final String ntpTime = getNtpTime();
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                ntpTimeTextView.setText("NTP Time: " + ntpTime);
+                            }
+                        });
+                    }
+                }).start();
             }
         });
+    }
+
+    private String getNtpTime() {
+        NTPUDPClient client = new NTPUDPClient();
+        client.setDefaultTimeout(5000);
+
+        try {
+            client.open();
+            TimeInfo timeInfo = client.getTime(InetAddress.getByName("pool.ntp.org"));
+
+            long ntpTimeMillis = timeInfo.getMessage().getTransmitTimeStamp().getTime();
+            Date ntpDate = new Date(ntpTimeMillis);
+            SimpleDateFormat ntpDateFormat = new SimpleDateFormat("HH:mm:ss", Locale.getDefault());
+
+            return ntpDateFormat.format(ntpDate);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "I'm offline!";
+        } finally {
+            client.close();
+        }
     }
 }
